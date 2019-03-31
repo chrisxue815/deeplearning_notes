a5701f0430772d32aa5b5894fb3ba54b32dbc115
Chris Xue <chrisxue815@gmail.com>
2019-03-31 16:24:15 +0100

    Tunning parameter initialization

--- course.1.week.4/assignment4.py
+++ course.1.week.4/assignment4.py
@@ -16,13 +16,13 @@ def initialize_parameters_deep(layer_dims):
                     bl -- bias vector of shape (layer_dims[l], 1)
     """
 
-    np.random.seed(3)
+    np.random.seed(1)
     parameters = {}
     L = len(layer_dims)            # number of layers in the network
 
     for l in range(1, L):
         ### START CODE HERE ### (≈ 2 lines of code)
-        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * 0.01
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) / np.sqrt(layer_dims[l-1])
         parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
         ### END CODE HERE ###
 
